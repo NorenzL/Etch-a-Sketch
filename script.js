1f2e09ca5f695fdc1598f286babd087800fb10df
Norenz Laurito <laurito.marknorenz@gmail.com>
2024-03-01 00:34:20 +0800

    Add change grid size button

--- script.js
+++ script.js
@@ -2,32 +2,52 @@ const container = document.querySelector(".container");
 
 let size = 16;
 
-for (let i = 1; i <= size; i++) {
-  const row = document.createElement("div");
-  const gridClass = document.createAttribute("class");
-  gridClass.value = "grid";
+function createGrid() {
+  for (let i = 1; i <= size; i++) {
+    const row = document.createElement("div");
+    const gridClass = document.createAttribute("class");
+    gridClass.value = "grid";
 
-  row.setAttributeNode(gridClass);
+    row.setAttributeNode(gridClass);
 
-  for (let j = 1; j <= size; j++) {
-    const col = document.createElement("div");
-    const colClass = document.createAttribute("class");
-    colClass.value = "col";
-    col.setAttributeNode(colClass);
-    row.appendChild(col);
+    for (let j = 1; j <= size; j++) {
+      const col = document.createElement("div");
+      col.setAttribute("class", "col");
+      row.appendChild(col);
+    }
+
+    container.appendChild(row);
   }
-
-  container.appendChild(row);
 }
 
-const col = document.querySelectorAll(".col");
+createGrid();
+createTrail();
 
-col.forEach((cols) => {
-  cols.addEventListener("mouseenter", () => {
-    let randomRed = Math.floor(Math.random() * 256);
-    let randomGreen = Math.floor(Math.random() * 256);
-    let randomBlue = Math.floor(Math.random() * 256);
+function createTrail() {
+  const col = document.querySelectorAll(".col");
 
-    cols.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+  col.forEach((cols) => {
+    cols.addEventListener("mouseenter", () => {
+      let randomRed = Math.floor(Math.random() * 256);
+      let randomGreen = Math.floor(Math.random() * 256);
+      let randomBlue = Math.floor(Math.random() * 256);
+
+      cols.style.backgroundColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+    });
   });
+}
+
+const resetBtn = document.querySelector(".btn");
+
+resetBtn.addEventListener("click", () => {
+  size = Number(
+    prompt("Enter a number for the size of the grid. Maximum of 100.")
+  );
+
+  // Removes grid
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  createGrid();
+  createTrail();
 });
